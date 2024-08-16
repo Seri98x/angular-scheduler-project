b07637012eae20264d893e25d19f4d4d825307c5
xSeri98 <ronquillo.236225@pasay-edsa.sti.edu.ph>
2024-08-16 17:19:23 +0800

    Added Ionic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { IonButton, IonContent,IonActionSheet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [IonButton,IonActionSheet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 }
